d90ed4fee47ed00f10c3b5aeed24b25b7919e2be
sungwoo-jo <sungwoo9671@naver.com>
2025-02-22 06:56:01 +0900

    회원가입 시 추가한 이미지를 가져오기 위한 ResourceHandler 설정 코드 추가

--- newProject/src/main/java/com/sw/newProject/config/WebConfig.java
+++ newProject/src/main/java/com/sw/newProject/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.sw.newProject.config;
 import com.sw.newProject.interceptor.LoginCheckInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
@@ -14,5 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
 //                .order(1)
 //                .addPathPatterns("/**") // 인터셉터 적용 페이지(전부)
 //                .excludePathPatterns("/", "/member/login", "/member/doLogin", "/member/join", "/member/doJoin", "/member/findId", "/member/findPw", "/member/doFindId", "/member/doFindPw", "/member/logout", "/css/**", "/script/**"); // 인터셉터 적용 예외 페이지
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/upload/**")
+                .addResourceLocations("file:C:/Users/sungw/OneDrive/바탕 화면/new 프로젝트/newProject/newProject/src/main/resources/static/upload");
     }
 }
